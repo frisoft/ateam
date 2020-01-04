6422decee8cb34b1238bda3ac3c7bdad255e3de2
Andrea Frigido <andrea@frisoft.it>
2020-01-04 00:00:55 +0000

    Tune score formula

--- src/types.rs
+++ src/types.rs
@@ -38,10 +38,10 @@ fn age(date_time: Option<DateTime<Utc>>) -> i64 {
 impl Score {
     pub fn from_pr(pr: &Pr) -> Score {
         Score {
-            age: age(pr.last_commit_pushed_date) as f64 * 10.0,
-            tests_result: (pr.tests_result - 1) as f64 * -2000.0,
+            age: age(pr.last_commit_pushed_date) as f64 * 0.5,
+            tests_result: (pr.tests_result - 1) as f64 * -200.0,
             open_conversations: pr.open_conversations as f64 * -20.0,
-            num_approvals: ((pr.num_approvals - 1) ^ 2) as f64 * -500.0,
+            num_approvals: ((pr.num_approvals - 1) ^ 2) as f64 * -50.0,
             num_reviewers: ((pr.num_reviewers - 1) ^ 2) as f64 * -20.0,
             additions: pr.additions as f64 * -0.5,
             deletions: pr.deletions as f64 * -0.1,
