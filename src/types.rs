3a4d306c0b8c5772ea9dab3472015879c8c1e9a7
Andrea Frigido <andrea@frisoft.it>
2020-12-13 15:07:50 +0000

    Coloured labels

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,7 @@
 use chrono::prelude::{DateTime, Utc};
 
+use crossterm::style::{Color, Colors, ResetColor, SetColors};
+
 pub struct Pr<'a> {
     pub title: String,
     pub url: String,
@@ -88,6 +90,37 @@ impl std::fmt::Display for Pr<'_> {
             // self.num_reviewers,
             // self.additions,
             // self.deletions,
+        )
+    }
+}
+
+impl std::fmt::Display for Labels<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.0
+                .iter()
+                .map(|label| label.to_string())
+                .collect::<Vec<String>>()
+                .join(" "),
+        )
+    }
+}
+
+impl std::fmt::Display for Label<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let c: u32 = u32::from_str_radix(self.color, 16).unwrap_or(0);
+        let (r, g, b) = ((c >> 16) as u8, ((c >> 8) & 0xff) as u8, (c & 0xff) as u8);
+        write!(
+            f,
+            "{} {} {}",
+            SetColors(Colors::new(
+                Color::Rgb { r: 0, g: 0, b: 0 },
+                Color::Rgb { r, g, b }
+            )),
+            self.name,
+            ResetColor
         )
     }
 }
