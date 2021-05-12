d6738f288fc20347ff41eb9ec9aeb61f682eed2b
Andrea Frigido <andrea@frisoft.it>
2021-05-12 22:20:52 +0100

    Small improvement of the release script

--- release-apple-darwin.sh
+++ release-apple-darwin.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 ver=$1
 targetdir="target/x86_64-apple-darwin/release"
-rm -rf $targetdir/ateam
+rm -rf $targetdir
 cargo build --release --target x86_64-apple-darwin
 strip $targetdir/ateam
 tar czf $targetdir/ateam-$ver-x86_64-apple-darwin.tar.gz -C $targetdir ateam
