4b3cd9cc999ed9188e7807cb875e514345f49d36
Andrea Frigido <andrea@frisoft.it>
2024-05-24 15:27:37 +0100

    Add build to flake.nix

--- flake.nix
+++ flake.nix
@@ -11,18 +11,26 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        deps = with pkgs;
+          [ cargo rustc pkg-config openssl cacert libiconv ]
+          ++ pkgs.lib.optionals pkgs.hostPlatform.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            pkgs.darwin.apple_sdk.frameworks.CoreServices
+            pkgs.darwin.apple_sdk.frameworks.Security
+          ];
       in {
         devShell = with pkgs;
           mkShell {
             buildInputs = with pkgs;
               [
                 # rustup
-                cargo
-                rustc
-                libiconv
-                pkg-config
-                openssl
+                # cargo
+                # rustc
+                # libiconv
+                # pkg-config
+                # openssl
                 # rust-analyzer <-- This does not work. You need to run `rustup component add rust-analyzer`
                 # tools for ./maintenance.sh
                 cargo-udeps
@@ -30,11 +38,33 @@
                 cargo-audit
                 cargo-dist
                 cargo-release
-              ] ++ lib.optionals hostPlatform.isDarwin [
-                darwin.apple_sdk.frameworks.SystemConfiguration
-                darwin.apple_sdk.frameworks.CoreServices
-                darwin.apple_sdk.frameworks.Security
-              ];
+                clippy
+              ] ++ deps;
           };
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "ateam";
+          version = "1.0.7";
+          src = ./.;
+          buildInputs = deps;
+
+          buildPhase = ''
+            export CARGO_NET_GIT_FETCH_WITH_CLI=true
+            export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+            export CARGO_HOME=$TMPDIR/cargo
+            cargo build --release
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/release/ateam $out/bin/
+          '';
+
+          meta = with pkgs.lib; {
+            description =
+              "ateam. The tool that helps optimize the code review process.";
+            license = licenses.mit;
+            maintainers = with maintainers; [ "frisoft" ];
+          };
+        };
       });
 }
