a23bde43180198514df0148b7b2cdcd67758a64d
Andrea Frigido <andrea@frisoft.it>
2025-09-12 11:28:19 +0100

    Update Nix conf

--- default.nix
+++ default.nix
@@ -9,11 +9,6 @@ rustPlatform.buildRustPackage rec {
   cargoLock = { lockFile = ./Cargo.lock; };
 
   nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ ] ++ lib.optionals darwin.apple_sdk.stdenv.isDarwin [
-    darwin.apple_sdk.frameworks.SystemConfiguration
-    darwin.apple_sdk.frameworks.CoreServices
-    darwin.apple_sdk.frameworks.Security
-  ];
 
   meta = with lib; {
     description = "ateam: The tool that helps optimize the code review process";
